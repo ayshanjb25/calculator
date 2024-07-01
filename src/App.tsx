4406219c07b599f3a89abe57e4ecc5e869ca6a48
Aisha Najeeb <90481018+ayshanjb25@users.noreply.github.com>
2024-07-01 18:38:37 +0530

    calculator base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
+import { Container, Grid, Paper, styled } from '@mui/material';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+const OutputContainer = styled('div')(({theme})=>({
+  width:"100%",
+  textAlign:"right",
+  height:"2em",
+  padding:theme.spacing(2),
+  fontSize:"3em",
+  overflow:"hidden"
+}))
+
+const CalculatorBase = styled(Paper)(({ theme})=>({
+  padding:theme.spacing(2),
+  marginTop:theme.spacing(4),
+  borderRadius:15
+}))
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+    <CalculatorBase elevation={3}>
+      <Grid container spacing={1}>
+      <Grid item xs={12}><OutputContainer>0</OutputContainer>
+        
+        </Grid>
+      </Grid>
+    </CalculatorBase>
+    </Container>
   );
 }
 
